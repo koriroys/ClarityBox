b141f51c66c65979350f1a4e235f31b99ddbbea5
Claire Lew <clew23@gmail.com>
2013-04-17 17:31:40 -0500

    Second design and test pages

---
+++ app/controllers/test_controller.rb
@@ -0,0 +1,10 @@
+class TestController < ApplicationController
+	def index
+	end
+
+	def story
+	end
+
+	def philosophy
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 ClarityBox::Application.routes.draw do
-  get "pages/home"
-  root :to => 'pages#home'
+
+	get('/', {:controller => 'pages', :action => 'home'})
+
+	get('/test', {:controller => 'Test', :action => 'index'})
+	get('/test/story', {:controller => 'Test', :action => 'story'})
+	get('/test/philosophy', {:controller => 'Test', :action => 'philosophy'})
+
 end
+
+
