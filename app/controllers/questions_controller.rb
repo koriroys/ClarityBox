f2f72c013c396b18d56a9aa68b29662595ab5f95
Claire Lew <clew23@gmail.com>
2013-05-01 14:02:20 -0500

    edited Questions controller

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,24 +1,48 @@
 class QuestionsController < ApplicationController
 
   def index
+    @questions = Question.all
   end
 
   def show
+    @question = Question.find_by_id(params[:id])
   end
 
   def new
+    @question = Question.new
   end
 
   def create
+    @question = Question.new
+    @question.question_text = params[:question_text]
+    @question.week_id = params[:week_id]
+
+    if @question.save
+            redirect_to questions_url
+          else
+      render 'new'
+    end
   end
 
   def edit
+    @question = Question.find_by_id(params[:id])
   end
 
   def update
+    @question = Question.find_by_id(params[:id])
+    @question.question_text = params[:question_text]
+    @question.week_id = params[:week_id]
+
+    if @question.save
+            redirect_to questions_url
+          else
+      render 'edit'
+    end
   end
 
   def destroy
-  end
-
+    @question = Question.find_by_id(params[:id])
+    @question.destroy
+        redirect_to questions_url
+      end
 end
