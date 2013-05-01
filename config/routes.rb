4e174e3a0b0e0df1bcf3278a0a46e73ddca33bdf
Claire Lew <clew23@gmail.com>
2013-05-01 13:36:02 -0500

    routed Questions and Responses

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,17 @@ ClarityBox::Application.routes.draw do
 # -- Routes for Homepage
 
 	get '/', :controller => 'Pages', :action => 'index'
-
+  #------------------------------
 
 # -- Routes for About pages
 
 	get '/about', :controller => 'Pages', :action => 'about'
 	get '/story', :controller => 'Pages', :action => 'story'
 	get '/philosophy', :controller => 'Pages', :action => 'philosophy'
+
+    #------------------------------
+
+
 
  # Routes for the Week resource:
   # CREATE
@@ -27,6 +31,46 @@ ClarityBox::Application.routes.draw do
   # DELETE
   delete '/weeks/:id', controller: 'Weeks', action: 'destroy'
   #------------------------------
+
+
+
+ # Routes for the Question resource:
+  # CREATE
+  get '/questions/new', controller: 'Questions', action: 'new', as: 'new_question'
+  post '/questions', controller: 'Questions', action: 'create'
+
+  # READ
+  get '/questions', controller: 'Questions', action: 'index', as: 'questions'
+  get '/questions/:id', controller: 'Questions', action: 'show', as: 'question'
+
+  # UPDATE
+  get '/questions/:id/edit', controller: 'Questions', action: 'edit', as: 'edit_question'
+  put '/questions/:id', controller: 'Questions', action: 'update'
+
+  # DELETE
+  delete '/questions/:id', controller: 'Questions', action: 'destroy'
+  #------------------------------
+
+
+
+ # Routes for the Response resource:
+  # CREATE
+  get '/responses/new', controller: 'Responses', action: 'new', as: 'new_response'
+  post '/responses', controller: 'Responses', action: 'create'
+
+  # READ
+  get '/responses', controller: 'Responses', action: 'index', as: 'responses'
+  get '/responses/:id', controller: 'Responses', action: 'show', as: 'responses'
+
+  # UPDATE
+  get '/responses/:id/edit', controller: 'Responses', action: 'edit', as: 'edit_response'
+  put '/responses/:id', controller: 'Responses', action: 'update'
+
+  # DELETE
+  delete '/responses/:id', controller: 'Responses', action: 'destroy'
+  #------------------------------
+
+
 
 
 
