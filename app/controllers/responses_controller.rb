236595a81aa244948b3d5af54681ab0199b1875d
Claire Lew <clew23@gmail.com>
2013-05-01 14:06:58 -0500

    edited Responses controller

--- app/controllers/responses_controller.rb
+++ app/controllers/responses_controller.rb
@@ -1,24 +1,50 @@
 class ResponsesController < ApplicationController
 
   def index
+    @responses = Response.all
   end
 
   def show
+    @responses = Response.find_by_id(params[:id])
   end
 
   def new
+    @response = Response.new
   end
 
   def create
+    @response = Role.new
+    @response.response_text = params[:response_text]
+    @response.user_name= params[:user_name]
+    @response.question_id = params[:question_id]
+
+    if @response.save
+            redirect_to responses_url
+          else
+      render 'new'
+    end
   end
 
   def edit
+    @response = Response.find_by_id(params[:id])
   end
 
   def update
+    @response = Response.find_by_id(params[:id])
+    @response.response_text = params[:response_text]
+    @response.user_name= params[:user_name]
+    @response.question_id = params[:question_id]
+
+    if @response.save
+            redirect_to responses_url
+          else
+      render 'edit'
+    end
   end
 
   def destroy
-  end
-
+    @response = Response.find_by_id(params[:id])
+    @response.destroy
+        redirect_to responses_url
+      end
 end
--- app/models/response.rb
+++ app/models/response.rb
@@ -2,5 +2,6 @@ class Response < ActiveRecord::Base
   attr_accessible :question_id, :response_text, :user_name
 
   belongs_to :question
+  belongs_to :week
 
 end
