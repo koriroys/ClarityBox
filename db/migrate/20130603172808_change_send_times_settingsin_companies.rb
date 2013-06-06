2803c0b53dafe694ddfeacba1c025509343e6bac
Claire Lew <clew23@gmail.com>
2013-06-06 09:05:56 -0500

    changed migration file

--- db/migrate/20130603172808_change_send_times_settingsin_companies.rb
+++ db/migrate/20130603172808_change_send_times_settingsin_companies.rb
@@ -1,19 +1,19 @@
 class ChangeSendTimesSettingsinCompanies < ActiveRecord::Migration
-  def up
-    add_column :companies, :send_question_day_of_week, :integer
-    add_column :companies, :send_question_time, :time
-    add_column :companies, :send_reminder_day_of_week, :integer
-    add_column :companies, :send_reminder_time, :time
-    add_column :companies, :send_rollup_day_of_week, :integer
-    add_column :companies, :send_rollup_time, :time
-  end
-  def down
-    remove_column :companies, :send_question_date, :datetime
-    remove_column :companies, :send_question_time, :datetime
-    remove_column :companies, :send_reminder_date, :datetime
-    remove_column :companies, :send_reminder_time, :datetime
-    remove_column :companies, :send_rollup_date, :datetime
-    remove_column :companies, :send_rollup_time, :datetime
-  end
+def up
+  remove_column :companies, :send_question_day
+  remove_column :companies, :send_question_time
+  remove_column :companies, :send_reminder_day
+  remove_column :companies, :send_reminder_time
+  remove_column :companies, :send_rollup_day
+  remove_column :companies, :send_rollup_time
+  add_column :companies, :send_question_day, :integer
+  add_column :companies, :send_question_time, :time
+  add_column :companies, :send_reminder_day, :integer
+  add_column :companies, :send_reminder_time, :time
+  add_column :companies, :send_rollup_day, :integer
+  add_column :companies, :send_rollup_time, :time
+end
+
 
 end
+
