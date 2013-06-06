3a45e236c0ea51cce981e7bebcd8118d1de8b5e6
Claire Lew <clew23@gmail.com>
2013-06-05 21:43:20 -0500

    edited the migrations to try to fix heroku error

--- db/migrate/20130603172808_change_send_times_settingsin_companies.rb
+++ db/migrate/20130603172808_change_send_times_settingsin_companies.rb
@@ -1,10 +1,19 @@
 class ChangeSendTimesSettingsinCompanies < ActiveRecord::Migration
-  def change
-      change_column :companies, :send_question_date, :integer
-      change_column :companies, :send_question_time, :time
-      change_column :companies, :send_reminder_date, :integer
-      change_column :companies, :send_reminder_time, :time
-      change_column :companies, :send_rollup_date, :integer
-      change_column :companies, :send_rollup_time, :time
+  def up
+    add_column :companies, :send_question_day_of_week, :integer
+    add_column :companies, :send_question_time, :time
+    add_column :companies, :send_reminder_day_of_week, :integer
+    add_column :companies, :send_reminder_time, :time
+    add_column :companies, :send_rollup_day_of_week, :integer
+    add_column :companies, :send_rollup_time, :time
   end
+  def down
+    remove_column :companies, :send_question_date, :datetime
+    remove_column :companies, :send_question_time, :datetime
+    remove_column :companies, :send_reminder_date, :datetime
+    remove_column :companies, :send_reminder_time, :datetime
+    remove_column :companies, :send_rollup_date, :datetime
+    remove_column :companies, :send_rollup_time, :datetime
+  end
+
 end
