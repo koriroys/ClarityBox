3ad9b3165ffb0caf1110bfe9e258da0b54789fc9
Benjamin Block <bjblock@gmail.com>
2013-06-07 15:06:37 -0500

    fixes migration for changing company columns

--- db/migrate/20130603172808_change_send_times_settingsin_companies.rb
+++ db/migrate/20130603172808_change_send_times_settingsin_companies.rb
@@ -1,10 +1,10 @@
 class ChangeSendTimesSettingsinCompanies < ActiveRecord::Migration
 def change
-  remove_column :companies, :send_question_day_of_week
+  remove_column :companies, :send_question_date
   remove_column :companies, :send_question_time
-  remove_column :companies, :send_reminder_day_of_week
+  remove_column :companies, :send_reminder_date
   remove_column :companies, :send_reminder_time
-  remove_column :companies, :send_rollup_day_of_week
+  remove_column :companies, :send_rollup_date
   remove_column :companies, :send_rollup_time
   add_column :companies, :send_question_day, :integer
   add_column :companies, :send_question_time, :time
