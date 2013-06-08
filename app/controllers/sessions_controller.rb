fc8c96b0399d41837ceee745b7c5d949e21f704b
Claire Lew <clew23@gmail.com>
2013-06-08 01:51:40 -0500

    styled the app home views

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,7 +10,7 @@ class SessionsController < ApplicationController
 
       if u.present? && u.authenticate(params[:password])
         session[:user_id] = u.id
-        redirect_to app_home_url, notice: "You've signed in successfully."
+        redirect_to app_home_url
       else
        redirect_to new_session_url, notice: 'Please try signing in again.'
       end
@@ -20,6 +20,6 @@ class SessionsController < ApplicationController
 
   def destroy
     reset_session
-    redirect_to app_home_url, notice: "Thanks for stopping by."
+    redirect_to app_home_url
   end
 end
