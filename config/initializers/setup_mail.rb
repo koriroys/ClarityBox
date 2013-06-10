168c1b7b6e82435d7343c7da3f126a8fdf73754b
Claire Lew <clew23@gmail.com>
2013-06-10 15:25:51 -0500

    added environment variables to setup_mail

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,10 +1,20 @@
 if Rails.env.production?
   ActionMailer::Base.smtp_settings = {
+  :address              => "smtp.gmail.com",
+  :port                 => 587,
+  :domain               => "claritybox.co",
+  :user_name            => ENV['gmail_username'],
+  :password             => ENV['gmail_password'],
+  :authentication       => "plain",
+  :enable_starttls_auto => true
+}
+elsif Rails.env.development?
+    ActionMailer::Base.smtp_settings = {
   :address              => "localhost",
   :port                 => 1025,
   :domain               => "localhost:3000",
-  :user_name            => "claire@claritybox.co",
-  :password             => "thanksgiving2012",
+  :user_name            => ENV['gmail_username'],
+  :password             => ENV['gmail_password'],
   :authentication       => "plain",
   :enable_starttls_auto => true
 }
