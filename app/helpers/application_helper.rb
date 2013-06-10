73ee9f9cdb6144ac5cc4836a21f7073451bfb98a
Claire Lew <clew23@gmail.com>
2013-06-10 16:19:53 -0500

    added gravatar avatar helpler but commented it out

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,11 @@
 module ApplicationHelper
 
+  # def avatar_url(user)
+  #   gravatar_id = Digest::MD5::hexdigest(user.email).downcase
+  #   "http://gravatar.com/avatar/#{gravatar_id}.png"
+  # end
+
+
   def user_company?(company_id)
     current_user.company_id == company_id
   end
